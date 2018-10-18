b858fb33c01c330e90393489f543084e54a1bfe4
Rosie Davies <rosie.davies@kgb.com>
2018-10-18 15:55:16 +0100

    api_post_request_commit

--- features/step_definitions/REST_methods.rb
+++ features/step_definitions/REST_methods.rb
@@ -7,3 +7,19 @@ def send_get(host, path)
   request['Content-Type'] = 'application/json'
   @response = http.request(request)
 end
+
+def create_user
+  @user = User.new
+  JSON.generate(@user)
+end
+
+def send_post(host, path, json)
+  url = URI(host + path)
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  request = Net::HTTP::Post.new(url)
+  request['Content-Type'] = 'application/json'
+  request.body = json
+  @response = http.request(request)
+end
---
+++ features/step_definitions/user_address.rb
@@ -0,0 +1,11 @@
+class UserAddress
+
+  attr_accessor :house, :street, :city
+
+  def initialize
+    @house = '12'
+    @street = 'Long Street'
+    @city = 'Cardiff'
+  end
+
+end
